ca5297c56ba128dbb29b955c693f807e992a6225
Ardy Armando <ardybelian@gmail.com>
2020-02-13 08:37:30 +0700

    commit awal

---
+++ app/examples/create.table.js
@@ -0,0 +1,10 @@
+//app/examples/create.table.js
+const sql = require("../models/db");
+sql.query("CREATE TABLE books (id int NOT NULL AUTO_INCREMENT, "+ "title VARCHAR(255) NOT NULL, description VARCHAR(255), "+ "images VARCHAR(255), created_at TIMESTAMP "+ "DEFAULT CURRENT_TIMESTAMPT, PRIMARY KEY(id))",
+(err, res) => {
+    if (eer) {
+        console.log(err);
+    } else {
+        console.log("Table berhasil dibuat");
+    }
+});
